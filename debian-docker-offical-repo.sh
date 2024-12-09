53711775a5f317388a22b30294d3ca24c90c3e91
Naf <26268276+Naf-u@users.noreply.github.com>
2024-12-09 16:05:35 +1100

    Create debian-docker-offical-repo.sh

---
+++ debian-docker-offical-repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
